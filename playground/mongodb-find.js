007738fe6a1171fcb728cbe3fb354c52b6aa0d45
Sparsh <sparshddcse@gmail.com>
2018-08-30 11:12:44 +0530

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b86c6f343303a3ed4d3d4d4')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to find Todos', err);
+  // });
+
+  // db.collection('Todos').find({}).count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to find Todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Sparsh'
+  }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to find users');
+  });
+ // client.close();
+});
